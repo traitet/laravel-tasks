10b8d39ffdeb7f12d15004b382fa9e7bb3d650e8
Traitet <traitet_th@aisin-ap.com>
2022-09-20 03:54:04 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,65 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Models\Task;
-use Illuminate\Http\Request;
-
-/**
-    * Show Task Dashboard
-    */
-// Route::get('/', function () {
-//     error_log("INFO: get /");
-//     return view('tasks', [
-//         'tasks' => Task::orderBy('created_at', 'asc')->get()
-//     ]);
-// });
-// // add force
-// Route::get('/task', function () {
-//     error_log("INFO: get /");
-//     return view('tasks', [
-//         'tasks' => Task::orderBy('created_at', 'asc')->get()
-//     ]);
-// });
-
-// Route::get('/', function () {
-
-//     return view('welcome');
-
-// });
-
-Route::get('/', function() {
-    return 'hello world';
-    });
-
-/**
-    * Add New Task
-    */
-// Route::post('/task', function (Request $request) {
-//     error_log("INFO: post /task");
-//     $validator = Validator::make($request->all(), [
-//         'name' => 'required|max:255',
-//     ]);
-
-//     if ($validator->fails()) {
-//         error_log("ERROR: Add task failed.");
-//         return redirect('/')
-//             ->withInput()
-//             ->withErrors($validator);
-//     }
-
-//     $task = new Task;
-//     $task->name = $request->name;
-//     $task->save();
-
-//     return redirect('/');
-// });
-
-// /**
-//     * Delete Task
-//     */
-// Route::delete('/task/{id}', function ($id) {
-//     error_log('INFO: delete /task/'.$id);
-//     Task::findOrFail($id)->delete();
-
-//     return redirect('/');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
